2fa3386cdc17ea63ee82167d7f84c4d3a2547fcb
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-12-02 18:00:54 +0800

    update

--- partials/header.php
+++ partials/header.php
@@ -22,7 +22,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Apotek Website</title>
+    <title>Material Bangunan</title>
     <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/14cd2e1081.js" crossorigin="anonymous"></script>
